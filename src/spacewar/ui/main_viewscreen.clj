42420af6ed64cc81658e06477f4ed683c8358afc
Robert C. Martin <unclebob@cleancoder.com>
2018-09-07 15:08:41 -0500

    Bottom row indicators laid out.

--- src/spacewar/ui/main_viewscreen.clj
+++ src/spacewar/ui/main_viewscreen.clj
@@ -13,15 +13,42 @@
   (setup [_] (frame. state))
   (update-state [_] (frame. state)))
 
-(deftype bottom-lights [state]
+(deftype indicator-light [state]
   p/Drawable
   (draw [_]
     (let [{:keys [x y w h]} state]
-      (q/fill 150 150 150)
-      (q/no-stroke)
+      (q/stroke 0 0 0)
+      (q/stroke-weight 1)
+      (q/fill 255 255 0)
       (q/rect x y w h)))
 
-  (setup [_] (bottom-lights. state))
+  (setup [_] (indicator-light. state))
+  (update-state [_] (indicator-light. state)))
+
+(deftype bottom-lights [state]
+  p/Drawable
+  (draw [_]
+    (let [{:keys [x y w h indicators]} state]
+      (q/fill 150 150 150)
+      (q/no-stroke)
+      (q/rect x y w h)
+      (doseq [indicator indicators] (p/draw indicator))))
+
+  (setup [_]
+    (let [{:keys [x y w h]} state
+          number 14
+          gap 20
+          indicator-height 10
+          indicator-width 20
+          spacing (/ (- w gap gap indicator-width) (dec number))
+          indicator-y (+ y (/ (- h indicator-height) 2))
+          indicators (map #(->indicator-light {:x (+ x gap (* spacing %))
+                                               :y indicator-y
+                                               :w indicator-width
+                                               :h indicator-height})
+                          (range 0 number))
+          new-state (assoc state :indicators indicators)]
+      (bottom-lights. new-state)))
 
   (update-state [_] (bottom-lights. state)))
 
@@ -53,29 +80,34 @@
           panel-gap 50
           frame-width (- w left-margin right-margin)
           frame-height (- h bottom-margin)
-          frame (->frame {:x (+ x left-margin)
-                          :y y
-                          :h frame-height
-                          :w frame-width})
+          frame (p/setup
+                  (->frame {:x (+ x left-margin)
+                            :y y
+                            :h frame-height
+                            :w frame-width}))
 
           bottom-row-width (/ frame-width 2)
           bottom-row-left-offset (/ (- frame-width bottom-row-width) 2)
-          bottom-row (->bottom-lights {:x (+ x left-margin bottom-row-left-offset)
-                                       :y (+ y (- h bottom-margin) panel-gap)
-                                       :h 40
-                                       :w bottom-row-width})
+          bottom-row (p/setup
+                       (->bottom-lights {:x (+ x left-margin bottom-row-left-offset)
+                                         :y (+ y (- h bottom-margin) panel-gap)
+                                         :h 40
+                                         :w bottom-row-width}))
 
           side-panel-height (/ frame-height 2.5)
           side-panel-width 120
           side-panel-y (+ y (/ frame-height 5))
-          left-lights (->side-lights {:x (- (+ x left-margin) panel-gap side-panel-width)
+          left-lights (p/setup
+                        (->side-lights {:x (- (+ x left-margin) panel-gap side-panel-width)
                                       :y side-panel-y
                                       :h side-panel-height
-                                      :w side-panel-width})
-          right-lights (->side-lights {:x (+ x left-margin frame-width panel-gap)
+                                      :w side-panel-width}))
+
+          right-lights (p/setup
+                         (->side-lights {:x (+ x left-margin frame-width panel-gap)
                                        :y side-panel-y
                                        :w side-panel-width
-                                       :h side-panel-height})
+                                       :h side-panel-height}))
           new-state (assoc state :frame frame
                                  :bottom-row bottom-row
                                  :left-lights left-lights
