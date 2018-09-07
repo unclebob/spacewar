828d2a5a7d9f7d7a16adb525769f98ba9a10009c
Robert C. Martin <unclebob@cleancoder.com>
2018-09-07 12:38:03 -0500

    Main viewscreen complex contains frame.  Get rid of warnings.

--- src/spacewar/core.clj
+++ src/spacewar/core.clj
@@ -1,16 +1,19 @@
 (ns spacewar.core
   (:require [quil.core :as q]
             [quil.middleware :as m]
-            [spacewar.ui.main-viewscreen :as mv]
+            [spacewar.ui.main-viewscreen :as main-viewer]
             [spacewar.ui.protocols :as p]))
 
 (defn setup []
   (let [vmargin 50 hmargin 20]
     (q/frame-rate 30)
     (q/color-mode :rgb)
-    {:state (mv/->frame {:x hmargin :y vmargin
-                         :w (- (q/width) (* 2 hmargin))
-                         :h (- (q/height) (* 2 vmargin))})}))
+    (q/background 200 200 200)
+    {:state (p/setup
+              (main-viewer/->complex
+                {:x hmargin :y vmargin
+                 :w (- (q/width) (* 2 hmargin))
+                 :h (- (q/height) (* 2 vmargin))}))}))
 
 (defn update-state [{:keys [state]}]
   {:state (p/update-state state)})
@@ -18,6 +21,7 @@
 (defn draw-state [{:keys [state]}]
   (p/draw state))
 
+(declare space-war)
 (defn -main [& args]
   (q/defsketch space-war
                :title "Space War"
@@ -26,4 +30,5 @@
                :update update-state
                :draw draw-state
                :features [:keep-on-top]
-               :middleware [m/fun-mode]))
+               :middleware [m/fun-mode])
+  args)
--- src/spacewar/ui/main_viewscreen.clj
+++ src/spacewar/ui/main_viewscreen.clj
@@ -2,15 +2,29 @@
   (:require [quil.core :as q]
             [spacewar.ui.protocols :as p]))
 
-
 (deftype frame [state]
   p/Drawable
-  (draw [this]
+  (draw [_]
     (let [{:keys [x y w h]} state]
-      (q/background 200 200 200)
       (q/stroke 0 0 255)
       (q/stroke-weight 5)
       (q/rect x y w h 5)))
-  (setup [this])
-  (update-state [this] this))
+  (setup [_] (frame. state))
+  (update-state [_] (frame. state)))
+
+(deftype complex [state]
+  p/Drawable
+  (draw [_]
+    (p/draw (:frame state)))
+
+  (setup [_]
+    (let [{:keys [x y h w]} state
+          frame (->frame {:x x :y y :h h :w w})
+          new-state (assoc state :frame frame)]
+      (complex. new-state)))
+
+  (update-state [_] (complex. state)))
+
+
+
 
