caf3ab9d5c02a47cb50838432244683fa339f9b7
Robert C. Martin <unclebob@cleancoder.com>
2018-09-07 13:26:03 -0500

    Bottom row sized and positioned

--- src/spacewar/ui/main_viewscreen.clj
+++ src/spacewar/ui/main_viewscreen.clj
@@ -8,25 +8,48 @@
     (let [{:keys [x y w h]} state]
       (q/stroke 0 0 255)
       (q/stroke-weight 5)
+      (q/fill 0 0 0)
       (q/rect x y w h 5)))
   (setup [_] (frame. state))
   (update-state [_] (frame. state)))
 
+(deftype bottom-lights [state]
+  p/Drawable
+  (draw [_]
+    (let [{:keys [x y w h]} state]
+      (q/fill 150 150 150)
+      (q/no-stroke)
+      (q/rect x y w h)))
+
+  (setup [_] (bottom-lights. state))
+
+  (update-state [_] (bottom-lights. state)))
+
 (deftype complex [state]
   p/Drawable
   (draw [_]
-    (p/draw (:frame state)))
+    (p/draw (:frame state))
+    (p/draw (:bottom-row state)))
 
   (setup [_]
     (let [{:keys [x y h w]} state
-          left-panel 200
-          right-panel 200
-          bottom-panel 140
-          frame (->frame {:x (+ x left-panel)
+          left-margin 200
+          right-margin 200
+          bottom-margin 100
+          panel-gap 50
+          frame-width (- w left-margin right-margin)
+          frame (->frame {:x (+ x left-margin)
                           :y y
-                          :h (- h bottom-panel)
-                          :w (- w left-panel right-panel)})
-          new-state (assoc state :frame frame)]
+                          :h (- h bottom-margin)
+                          :w frame-width})
+          bottom-row-width (/ frame-width 2)
+          bottom-row-left-offset (/ (- frame-width bottom-row-width) 2)
+          bottom-row (->bottom-lights {:x (+ x left-margin bottom-row-left-offset)
+                                       :y (+ y (- h bottom-margin) panel-gap)
+                                       :h 40
+                                       :w bottom-row-width})
+          new-state (assoc state :frame frame
+                                 :bottom-row bottom-row)]
       (complex. new-state)))
 
   (update-state [_] (complex. state)))
