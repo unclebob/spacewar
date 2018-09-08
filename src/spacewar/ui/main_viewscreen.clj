143ede4639bd6a34e4dafe8751a72e8d0fbbd152
Robert C. Martin <unclebob@cleancoder.com>
2018-09-08 10:28:47 -0500

    Add on-func? to bottom-lights state to prepare for polymorphic lighting patterns.

--- src/spacewar/ui/main_viewscreen.clj
+++ src/spacewar/ui/main_viewscreen.clj
@@ -26,14 +26,26 @@
   (update-state [this] this)
   (get-state [_] state))
 
+(defn draw-light-panel [state]
+  (let [{:keys [x y w h indicators]} state]
+    (q/fill 150 150 150)
+    (q/no-stroke)
+    (q/rect x y w h)
+    (doseq [indicator indicators] (p/draw indicator))))
+
+(defn update-light-panel [state]
+  (let [{:keys [indicators on-func?]} state
+            indicator-states (map p/get-state indicators)
+            new-indicators (map-indexed #(->indicator-light (assoc %2 :on? (on-func? %1))) indicator-states)
+            new-state (assoc state :indicators new-indicators)]
+    new-state))
+
+(defn shift-pattern [n i]
+  (= i (rem (quot (q/frame-count) 3) n)))
+
 (deftype bottom-lights [state]
   p/Drawable
-  (draw [_]
-    (let [{:keys [x y w h indicators]} state]
-      (q/fill 150 150 150)
-      (q/no-stroke)
-      (q/rect x y w h)
-      (doseq [indicator indicators] (p/draw indicator))))
+  (draw [_] (draw-light-panel state))
 
   (setup [_]
     (let [{:keys [x y w h]} state
@@ -52,29 +64,19 @@
                                 :on? false?
                                 :draw-func q/rect}))
                           (range 0 number))
-          new-state (assoc state :indicators indicators)]
+          new-state (assoc state :indicators indicators
+                                 :on-func? (partial shift-pattern number))]
       (bottom-lights. new-state)))
 
-  (update-state [_]
-    (let [old-indicators (:indicators state)
-          n (count old-indicators)
-          on-index (rem (quot (q/frame-count) 3) n)
-          indicator-states (map p/get-state old-indicators)
-          indicators (map-indexed #(->indicator-light (assoc %2 :on? (= on-index %1))) indicator-states)
-          new-state (assoc state :indicators indicators)]
-      (bottom-lights. new-state))))
+  (update-state [_] (bottom-lights. (update-light-panel state))))
 
 (deftype side-lights [state]
   p/Drawable
-  (draw [_]
-    (let [{:keys [x y h w]} state]
-      (q/no-stroke)
-      (q/fill 200 50 50)
-      (q/rect x y w h)))
+  (draw [_] (draw-light-panel state))
 
   (setup [_] (side-lights. state))
 
-  (update-state [this] this))
+  (update-state [_] (bottom-lights. (update-light-panel state))))
 
 (deftype complex [state]
   p/Drawable
