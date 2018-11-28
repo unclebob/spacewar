8eae1a80a6e408a7d411d7e7f8a2c63091a0b13b
Robert C. Martin <unclebob@cleancoder.com>
2018-11-28 10:10:07 +0000

    eliminate :refer :all from stars

--- src/spacewar/game_logic/stars.clj
+++ src/spacewar/game_logic/stars.clj
@@ -1,10 +1,10 @@
 (ns spacewar.game-logic.stars
   (:require [clojure.spec.alpha :as s]
-            [spacewar.game-logic.config :refer :all]))
+            [spacewar.game-logic.config :as glc]))
 
 (s/def ::x number?)
 (s/def ::y number?)
-(s/def ::class (set spectral-classes))
+(s/def ::class (set glc/spectral-classes))
 (s/def ::star (s/keys :req-un [::x ::y ::class]))
 (s/def ::stars (s/coll-of ::star))
 
@@ -19,9 +19,9 @@
       :m))
 
 (defn make-random-star []
-  {:x (int (rand known-space-x))
-   :y (int (rand known-space-y))
+  {:x (int (rand glc/known-space-x))
+   :y (int (rand glc/known-space-y))
    :class (random-class)})
 
 (defn initialize []
-  (repeatedly number-of-stars make-random-star))
+  (repeatedly glc/number-of-stars make-random-star))
