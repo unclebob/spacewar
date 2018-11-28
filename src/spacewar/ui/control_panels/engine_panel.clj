38d502585eb7c14d64e3126cbd835fcea2af3394
Robert C. Martin <unclebob@cleancoder.com>
2018-11-28 12:55:03 +0000

    eliminate :refer :all from engine-panel

--- src/spacewar/ui/control_panels/engine_panel.clj
+++ src/spacewar/ui/control_panels/engine_panel.clj
@@ -1,12 +1,12 @@
 (ns spacewar.ui.control-panels.engine-panel
   (:require [spacewar.ui.protocols :as p]
-            [spacewar.ui.config :refer :all]
-            [spacewar.ui.widgets.lcars :refer :all]
-            [spacewar.ui.widgets.button :refer :all]
-            [spacewar.ui.widgets.direction-selector :refer :all]
-            [spacewar.ui.widgets.slider :refer :all]
-            [spacewar.ui.widgets.engage :refer :all]
-            [spacewar.geometry :refer :all]
+            [spacewar.ui.config :as uic]
+            [spacewar.ui.widgets.lcars :as lcars]
+            [spacewar.ui.widgets.button :refer [->button]]
+            [spacewar.ui.widgets.direction-selector :refer [->direction-selector]]
+            [spacewar.ui.widgets.slider :refer [->slider]]
+            [spacewar.ui.widgets.engage :refer [->engage]]
+            [spacewar.geometry :as geo]
             [spacewar.vector :as vector]
             [spacewar.game-logic.ship :as ship]
             [quil.core :as q]))
@@ -18,9 +18,9 @@
 
   (draw [_]
     (let [{:keys [heading-label-x power-label-x label-y]} state]
-      (draw-banner state)
+      (lcars/draw-banner state)
       (p/draw-elements state)
-      (apply q/fill black)
+      (apply q/fill uic/black)
       (q/text-align :center :bottom)
       (q/text-font (:lcars (q/state :fonts)) 14)
       (q/text "HEADING" heading-label-x label-y)
@@ -29,24 +29,24 @@
   (setup [_]
     (let [{:keys [x y w h color button-color]} state
           button-w 150
-          warp-y (+ y banner-width button-gap)
-          impulse-y (+ warp-y button-h button-gap)
-          dock-y (+ impulse-y button-h button-gap)
-          direction-x (+ x button-w button-gap)
-          direction-y (+ y banner-width button-gap)
-          direction-diameter (- h banner-width)
-          power-x (+ direction-x direction-diameter button-gap)
+          warp-y (+ y uic/banner-width uic/button-gap)
+          impulse-y (+ warp-y uic/button-h uic/button-gap)
+          dock-y (+ impulse-y uic/button-h uic/button-gap)
+          direction-x (+ x button-w uic/button-gap)
+          direction-y (+ y uic/banner-width uic/button-gap)
+          direction-diameter (- h uic/banner-width)
+          power-x (+ direction-x direction-diameter uic/button-gap)
           power-y direction-y
-          power-w slider-width
-          power-h (- h banner-width)
-          engage-x (- (+ x w) stringer-width button-gap engage-width)
+          power-w uic/slider-width
+          power-h (- h uic/banner-width)
+          engage-x (- (+ x w) uic/stringer-width uic/button-gap uic/engage-width)
           engage-y direction-y
-          engage-w engage-width
-          engage-h (- h banner-width)
+          engage-w uic/engage-width
+          engage-h (- h uic/banner-width)
 
           heading-label-x (+ direction-x (/ direction-diameter 2))
           power-label-x (+ power-x (/ power-w 2))
-          label-y (+ y banner-width)]
+          label-y (+ y uic/banner-width)]
 
       (engine-panel.
         (assoc state
@@ -58,7 +58,7 @@
                     {:x x
                      :y warp-y
                      :w button-w
-                     :h button-h
+                     :h uic/button-h
                      :name "WARP"
                      :color button-color
                      :left-up-event {:event :select-warp}}))
@@ -67,7 +67,7 @@
                        {:x x
                         :y impulse-y
                         :w button-w
-                        :h button-h
+                        :h uic/button-h
                         :name "IMPULSE"
                         :color button-color
                         :left-up-event {:event :select-impulse}}))
@@ -76,7 +76,7 @@
                     {:x x
                      :y dock-y
                      :w button-w
-                     :h button-h
+                     :h uic/button-h
                      :name "DOCK"
                      :color button-color
                      :disabled true
@@ -122,11 +122,11 @@
           {:keys [heading heading-setting velocity selected-engine engine-power-setting warp]} ship
           selected-engine (or selected-engine :none)
           warp-color (if (= selected-engine :warp)
-                       engine-panel-selection-color
-                       engine-panel-button-color)
+                       uic/engine-panel-selection-color
+                       uic/engine-panel-button-color)
           impulse-color (if (= selected-engine :impulse)
-                          engine-panel-selection-color
-                          engine-panel-button-color)
+                          uic/engine-panel-selection-color
+                          uic/engine-panel-button-color)
           engage-disabled (= selected-engine :none)
           dockable (ship/dockable? ship bases)
 
@@ -134,7 +134,7 @@
                   state
                   [[:dock :disabled (not dockable)]
                    [:direction-selector :pointer2 heading]
-                   [:impulse :status (str (round (* 20 (vector/magnitude velocity))))]
+                   [:impulse :status (str (geo/round (* 20 (vector/magnitude velocity))))]
                    [:impulse :color impulse-color]
                    [:warp :color warp-color]
                    [:power-slider :value engine-power-setting]
