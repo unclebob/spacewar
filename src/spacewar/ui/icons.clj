b79d7f4368c767ebb19b02e5153c14c01341f68d
Robert C. Martin <unclebob@cleancoder.com>
2018-12-01 12:30:05 +0000

    eliminate :refer :all from icons

--- src/spacewar/ui/icons.clj
+++ src/spacewar/ui/icons.clj
@@ -1,16 +1,15 @@
 (ns spacewar.ui.icons
   (:require [quil.core :as q]
-            [spacewar.util :refer :all]
-            [spacewar.ui.config :refer :all]
-            [spacewar.game-logic.config :refer :all]
-            [spacewar.geometry :refer :all]
+            [spacewar.ui.config :as uic]
+            [spacewar.game-logic.config :as glc]
+            [spacewar.geometry :as geo]
             [spacewar.vector :as vector]
             [spacewar.util :as util]))
 
 (defn- transport-color [commodity]
   (condp = commodity
-    :antimatter orange
-    :dilithium yellow))
+    :antimatter uic/orange
+    :dilithium uic/yellow))
 
 (defn draw-transport-icon [transport]
   (q/ellipse-mode :center)
@@ -19,7 +18,7 @@
   (q/triangle 0 -8 -6 6 6 6))
 
 (defn- age-angle [age]
-  (let [maturity (min 1 (/ age base-maturity-age))]
+  (let [maturity (min 1 (/ age glc/base-maturity-age))]
     (* (- 1 maturity) 2 Math/PI)))
 
 (defn- draw-base-age [age]
@@ -29,19 +28,19 @@
   (q/arc 0 0 30 30 0 (age-angle age) :pie))
 
 (defn- draw-base-contents [antimatter dilithium]
-  (let [antimatter-angle (* 2 Math/PI (/ antimatter base-antimatter-maximum))
-        dilithium-angle (* 2 Math/PI (/ dilithium base-dilithium-maximum))]
+  (let [antimatter-angle (* 2 Math/PI (/ antimatter glc/base-antimatter-maximum))
+        dilithium-angle (* 2 Math/PI (/ dilithium glc/base-dilithium-maximum))]
     (q/stroke-weight 3)
     (q/no-fill)
     (when (> dilithium-angle 0.01)
-      (apply q/stroke yellow)
+      (apply q/stroke uic/yellow)
       (q/arc 0 0 30 30 0 dilithium-angle))
     (when (> antimatter-angle 0.01)
-      (apply q/stroke orange)
+      (apply q/stroke uic/orange)
       (q/arc 0 0 35 35 0 antimatter-angle))))
 
 (defn- draw-base-counts [base]
-  (apply q/fill white)
+  (apply q/fill uic/white)
   (q/text-align :right :center)
   (q/text-font (:lcars-small (q/state :fonts)) 12)
   (q/text (str "T-" (int (:torpedos base))) -30 0)
@@ -80,29 +79,29 @@
 
 (defmethod draw-romulan :invisible [romulan]
   (q/no-stroke)
-  (q/fill 255 255 255 (min 15 (* 15 (/ (:age romulan) romulan-invisible-time))))
+  (q/fill 255 255 255 (min 15 (* 15 (/ (:age romulan) glc/romulan-invisible-time))))
   (q/ellipse 0 0 30 30)
   )
 
 (defmethod draw-romulan :appearing [romulan]
-  (apply q/stroke (conj white (min 255 (* 255 (/ (:age romulan) romulan-appearing-time)))))
+  (apply q/stroke (conj uic/white (min 255 (* 255 (/ (:age romulan) glc/romulan-appearing-time)))))
   (draw-romulan-icon)
   )
 
 (defmethod draw-romulan :visible [romulan]
-  (apply q/stroke (color-shift white orange (min 1 (/ (:age romulan) romulan-visible-time))))
+  (apply q/stroke (util/color-shift uic/white uic/orange (min 1 (/ (:age romulan) glc/romulan-visible-time))))
   (draw-romulan-icon)
   )
 
 (defmethod draw-romulan :firing [romulan]
-  (apply q/stroke (color-shift orange red (min 1 (/ (:age romulan) romulan-firing-time))))
+  (apply q/stroke (util/color-shift uic/orange uic/red (min 1 (/ (:age romulan) glc/romulan-firing-time))))
   (doseq [_ (range 10)]
     (q/line 0 0 (- 30 (rand 60)) (- 30 (rand 60))))
   (draw-romulan-icon)
   )
 
 (defmethod draw-romulan :fading [romulan]
-  (apply q/stroke (color-shift red black (min 1 (/ (:age romulan) romulan-fading-time))))
+  (apply q/stroke (util/color-shift uic/red uic/black (min 1 (/ (:age romulan) glc/romulan-fading-time))))
   (draw-romulan-icon)
   )
 
@@ -110,14 +109,14 @@
   )
 
 (defn draw-strategic-romulan []
-  (apply q/stroke orange)
+  (apply q/stroke uic/orange)
   (draw-romulan-icon))
 
 (defmulti draw-base-icon :type)
 
 (defmethod draw-base-icon :weapon-factory [base]
   (q/no-fill)
-  (apply q/stroke weapon-factory-color)
+  (apply q/stroke uic/weapon-factory-color)
   (q/stroke-weight 2)
   (q/ellipse-mode :center)
   (q/ellipse 0 0 12 12)
@@ -128,7 +127,7 @@
 
 (defmethod draw-base-icon :antimatter-factory [base]
   (q/no-fill)
-  (apply q/stroke antimatter-factory-color)
+  (apply q/stroke uic/antimatter-factory-color)
   (q/stroke-weight 2)
   (q/ellipse-mode :center)
   (q/ellipse 0 0 12 12)
@@ -143,7 +142,7 @@
 
 (defmethod draw-base-icon :dilithium-factory [base]
   (q/no-fill)
-  (apply q/stroke dilithium-factory-color)
+  (apply q/stroke uic/dilithium-factory-color)
   (q/stroke-weight 2)
   (q/ellipse-mode :center)
   (q/quad 0 6 6 0 0 -6 -6 0)
@@ -157,8 +156,8 @@
   (draw-base-adornments base))
 
 (defn draw-klingon-icon []
-  (apply q/fill black)
-  (apply q/stroke klingon-color)
+  (apply q/fill uic/black)
+  (apply q/stroke uic/klingon-color)
   (q/stroke-weight 2)
   (q/ellipse-mode :center)
   (q/line 0 0 10 -6)
@@ -168,8 +167,8 @@
   (q/ellipse 0 0 6 6))
 
 (defn draw-klingon-shields [shields]
-  (when (< shields klingon-shields)
-    (let [pct (/ shields klingon-shields)
+  (when (< shields glc/klingon-shields)
+    (let [pct (/ shields glc/klingon-shields)
           flicker (< (rand 3) pct)
           color [255 (* pct 255) 0 (if flicker (* pct 100) 100)]
           radius (+ 35 (* pct 20))]
@@ -179,15 +178,15 @@
       (q/ellipse 0 0 radius radius))))
 
 (defn draw-ship-icon [[vx vy] radians]
-  (apply q/stroke enterprise-vector-color)
+  (apply q/stroke uic/enterprise-vector-color)
   (q/stroke-weight 2)
   (q/line 0 0 vx vy)
   (q/with-rotation
     [radians]
-    (apply q/stroke enterprise-color)
+    (apply q/stroke uic/enterprise-color)
     (q/stroke-weight 2)
     (q/ellipse-mode :center)
-    (apply q/fill black)
+    (apply q/fill uic/black)
     (q/line -9 -9 0 0)
     (q/line -9 9 0 0)
     (q/ellipse 0 0 9 9)
@@ -198,8 +197,8 @@
   (let [class (:class star)]
     (q/no-stroke)
     (q/ellipse-mode :center)
-    (apply q/fill (class star-colors))
-    (q/ellipse 0 0 (class star-sizes) (class star-sizes))))
+    (apply q/fill (class uic/star-colors))
+    (q/ellipse 0 0 (class uic/star-sizes) (class uic/star-sizes))))
 
 (defn- draw-blob [jitter half-jitter diameter]
   (q/ellipse (- half-jitter (rand jitter))
@@ -208,7 +207,7 @@
   )
 
 (defn- draw-spark [x y]
-  (apply q/fill yellow)
+  (apply q/fill uic/yellow)
   (q/ellipse x y 1 1))
 
 (defn- rand-sign []
@@ -218,7 +217,7 @@
   (let [diameter (* 0.3 (:concentration cloud))
         jitter (/ diameter 5)
         half-jitter (/ jitter 2)]
-    (apply q/fill (conj yellow 10))
+    (apply q/fill (conj uic/yellow 10))
     (q/no-stroke)
     (q/ellipse-mode :center)
     (doseq [_ (range 10)]
@@ -233,7 +232,7 @@
         (draw-spark x y)))))
 
 (defn- romulan-blast-visual-intensity [range]
-  (let [factor (* 255 (- 1.0 (/ range romulan-blast-range)))]
+  (let [factor (* 255 (- 1.0 (/ range glc/romulan-blast-range)))]
     factor))
 
 (defn- romulan-blast-weight []
@@ -245,7 +244,7 @@
 (defn draw-romulan-shot [scale shot]
   (let [{:keys [range bearing]} shot
         scaled-range (* range scale)
-        radians (->radians bearing)
+        radians (geo/->radians bearing)
         radians-to-origin (+ radians Math/PI)
         shot-center (vector/from-angular scaled-range radians-to-origin)
         shot-x (first shot-center)
