a53e73cdb904d10c8d160300c466adec23f4381d
Robert C. Martin <unclebob@cleancoder.com>
2018-12-01 10:02:49 +0000

    eliminate :refer :all from direction-selector

--- src/spacewar/ui/widgets/direction_selector.clj
+++ src/spacewar/ui/widgets/direction_selector.clj
@@ -1,17 +1,17 @@
 (ns spacewar.ui.widgets.direction-selector
   (:require [quil.core :as q]
             [spacewar.ui.protocols :as p]
-            [spacewar.geometry :refer :all]
-            [spacewar.ui.config :refer :all]
+            [spacewar.geometry :as geo]
+            [spacewar.ui.config :as uic]
             [spacewar.vector :as vector]))
 
 (defn degree-tick [radius angle]
   (let [tick-length (if (zero? (rem angle 30)) 10 5)
         tick-radius (- radius tick-length)
-        radians (->radians angle)
+        radians (geo/->radians angle)
         sin-r (Math/sin radians)
         cos-r (Math/cos radians)]
-    (map #(round %)
+    (map #(geo/round %)
          [(* sin-r radius) (* cos-r radius)
           (* sin-r tick-radius) (* cos-r tick-radius)])))
 
@@ -23,7 +23,7 @@
   (q/ellipse cx cy radius radius))
 
 (defn draw-ticks [[x y radius]]
-  (apply q/stroke black)
+  (apply q/stroke uic/black)
   (doseq [angle-tenth (range 36)]
     (q/with-translation
       [x y]
@@ -33,19 +33,19 @@
   (doseq [angle-thirtieth (range 12)]
     (let [angle-tenth (* 3 angle-thirtieth)
           angle (* 10 angle-tenth)
-          radians (->radians angle)
+          radians (geo/->radians angle)
           [label-x label-y] (vector/from-angular radius radians)]
       (q/with-translation
         [x y]
-        (apply q/fill black)
+        (apply q/fill uic/black)
         (q/text-align :center :center)
         (q/text-font (:lcars-small (q/state :fonts)) 12)
         (q/text (str angle-tenth) label-x label-y)))))
 
 (defn draw-pointer [x y length direction color]
-  (let [radians (->radians direction)
+  (let [radians (geo/->radians direction)
         [tip-x tip-y] (vector/from-angular length radians)
-        base-width (->radians 10)
+        base-width (geo/->radians 10)
         base-offset 15
         [xb1 yb1] (vector/from-angular base-offset (- radians base-width))
         [xb2 yb2] (vector/from-angular base-offset (+ radians base-width))]
@@ -84,19 +84,19 @@
           [cx cy radius] circle
           label-radius (- radius 18)
           pointer-length (- radius 25)
-          ring-color (if mouse-in white color)
-          mouse-angle (if left-down (angle-degrees [cx cy] mouse-pos) 0)
-          direction-text (str (round (if left-down mouse-angle direction)))
-          text-color (if left-down grey black)]
+          ring-color (if mouse-in uic/white color)
+          mouse-angle (if left-down (geo/angle-degrees [cx cy] mouse-pos) 0)
+          direction-text (str (geo/round (if left-down mouse-angle direction)))
+          text-color (if left-down uic/grey uic/black)]
       (draw-bezel-ring circle ring-color color)
       (when mouse-in
         (draw-ticks circle)
         (draw-labels circle label-radius))
       (when pointer2
-        (draw-pointer cx cy pointer-length pointer2 light-grey))
-      (draw-pointer cx cy pointer-length direction black)
+        (draw-pointer cx cy pointer-length pointer2 uic/light-grey))
+      (draw-pointer cx cy pointer-length direction uic/black)
       (when left-down
-        (draw-pointer cx cy pointer-length mouse-angle grey))
+        (draw-pointer cx cy pointer-length mouse-angle uic/grey))
       (draw-direction-text direction-text cx cy color text-color)))
 
   (setup [_] (direction-selector. (assoc state :mouse-pos [0 0])))
@@ -106,11 +106,11 @@
           last-left-down (:left-down state)
           [cx cy _ :as circle] (->circle x y diameter)
           mouse-pos [(q/mouse-x) (q/mouse-y)]
-          mouse-in (inside-circle circle mouse-pos)
+          mouse-in (geo/inside-circle circle mouse-pos)
           left-down (and mouse-in (q/mouse-pressed?) (= :left (q/mouse-button)))
           left-up (and (not left-down) last-left-down mouse-in)
           event (if left-up
-                  (assoc (:left-up-event state) :angle (round (angle-degrees [cx cy] mouse-pos)))
+                  (assoc (:left-up-event state) :angle (geo/round (geo/angle-degrees [cx cy] mouse-pos)))
                   nil)
           new-state (assoc state
                       :mouse-pos mouse-pos
