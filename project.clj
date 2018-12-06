56db2df1785d8883a3551dbe6bb989c5b713cb59
Robert C. Martin <unclebob@cleancoder.com>
2018-12-06 12:23:40 -0600

    Add clojure/tools.reader to project.

--- project.clj
+++ project.clj
@@ -8,7 +8,8 @@
                  [figwheel-sidecar "0.5.15"]
                  [org.clojure/test.check "0.10.0-alpha3"]
                  [quil "2.7.1"]
-                 [org.clojure/math.combinatorics "0.1.4"]]
+                 [org.clojure/math.combinatorics "0.1.4"]
+                 [org.clojure/tools.reader "1.3.2"]]
   :plugins [[lein-cljsbuild "1.1.7"]
             [lein-figwheel "0.5.15"]]
   :profiles {:dev {:dependencies [[midje "1.9.2"]]}
